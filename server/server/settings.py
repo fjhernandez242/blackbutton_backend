f00024625aef2b2e30e8666debeac4d477f2cc72
fjhernandez242 <fjhernandez049@gmail.com>
2025-11-12 18:59:21 -0600

    prueba push

--- server/server/settings.py
+++ server/server/settings.py
@@ -140,6 +140,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
+# Prueba
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

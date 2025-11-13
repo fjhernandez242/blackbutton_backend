d6a7f4c2a38048cb995a8c4491c12ec08ea1a195
fjhernandez242 <fjhernandez049@gmail.com>
2025-11-12 19:09:38 -0600

    Prueba commit con ssh-rsa

--- server/server/settings.py
+++ server/server/settings.py
@@ -140,7 +140,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
-# Prueba
+#pruebas
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
